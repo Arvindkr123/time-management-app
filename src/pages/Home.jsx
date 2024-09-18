6eb20aa20ec95e47cb58a6bf6a72688ae6c8b061
arvind kumar <thakurarvindkr10@gmail.com>
2024-09-18 15:03:04 +0530

    added app for time table management system

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4 h-[calc(80vh)]">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+};
+
+export default Home;
